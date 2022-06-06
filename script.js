84e6762991fc30fa919e6eca1057d29ccbe1ea04
Franklin <Franklinau2@gmail.com>
2022-06-06 14:46:15 -0700

    Complete playRound() function

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // computerPlay to make the computer randomly play: rock, paper, or scissors.
 function computerPlay() {
-    const choice = ["Rock", "Paper", "Scissors"];
+    const choice = ["ROCK", "PAPER", "SCISSORS"];
    return choice[Math.floor(Math.random() * choice.length)];
 }
-console.log(computerPlay());
 
 /* Function that plays a round of the game
     Takes the player's selection of "Rock", "Paper", or "Scissors".
@@ -14,3 +13,16 @@ console.log(computerPlay());
         Scissors beats Paper
         Paper beats Rock
 */
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    // Compares playerSelection to computerSelection
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+    } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER") || 
+    (playerSelection === "PAPER" && computerSelection === "ROCK")) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+    } else {
+        console.log(`You lose! ${playerSelection} loses against ${computerSelection}!`);
+    }
+}
